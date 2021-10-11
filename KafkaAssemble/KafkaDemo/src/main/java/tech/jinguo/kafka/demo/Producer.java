e3b938bc47be8cd956f2e656b1dedf57bcc74971
qianjinguo <jinguo.qian@ly.com>
2021-10-11 16:05:38 +0800

    知识体系-Kafka体系-KafkaDemo

---
+++ KafkaAssemble/KafkaDemo/src/main/java/tech/jinguo/kafka/demo/Producer.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright: 2021 www.chebada.com Inc. All rights reserved.
+ * 注意：本内容仅限于车巴达公司内部传阅，禁止外泄以及用于其他的商业目的
+ *
+ * @Project: kafka
+ * @File: Producer
+ * @Package: tech.jinguo.kafka
+ * @Date: 2021/9/2 09:52
+ * @Author: by jinguo.qian
+ *
+ */
+package tech.jinguo.kafka.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author jinguo.qian
+ * @title: Producer
+ * @projectName kafka
+ * @description: 生产者
+ * @date 2021/9/2 9:52
+ */
+@RestController
+public class Producer {
+    @Autowired
+    private KafkaTemplate kafkaTemplate;
+
+    @GetMapping("send")
+    public void send(){
+
+        String message = "你好，我是Java旅途";
+        // 第一个参数 topic
+        // 第二个参数 消息
+        kafkaTemplate.send("first-topic",message);
+    }
+}
+    
