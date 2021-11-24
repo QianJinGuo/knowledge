a856372807006d8d8d1451816a04aaa8508989f4
jinguo.qian <1263639596@qq.com>
2021-11-25 00:31:35 +0800

    FactoryBean

---
+++ SpringAssemble/SpringDemo/src/main/java/tech/jinguo/springdemo/bean2/MyFactoryBean.java
@@ -0,0 +1,31 @@
+package tech.jinguo.springdemo.bean2;
+
+import org.springframework.beans.factory.FactoryBean;
+import tech.jinguo.springdemo.bean.Book;
+
+public class MyFactoryBean implements FactoryBean<Book> {
+
+
+    @Override
+    public Book getObject() throws Exception {
+        return new Book(8,"SpringDemo2","jinguo",8.88,888);
+    }
+
+    /**
+     * 设置返回的MYbean类型
+     * @return
+     */
+    @Override
+    public Class<?> getObjectType() {
+        return Book.class;
+    }
+
+    /**
+     * 是否单例
+     * @return
+     */
+    @Override
+    public boolean isSingleton() {
+        return true;
+    }
+}
