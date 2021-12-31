1b0b4b64d4e85997c96348c56725c98639f5c47f
jinguo.qian <1263639596@qq.com>
2022-01-01 01:49:46 +0800

    LinkedHashMap用做LRU缓存Demo

---
+++ TestModule/src/main/java/jinguo/tech/maputil/SimpleCacheTest.java
@@ -0,0 +1,23 @@
+package jinguo.tech.maputil;
+
+import java.util.stream.IntStream;
+
+/**
+ * @author jinguo
+ */
+public class SimpleCacheTest {
+    public static void main(String[] args) {
+        SimpleCache<Integer,Integer> cache = new SimpleCache<>(3);
+        IntStream.range(0,9).forEach(i->cache.save(i,i*i));
+        System.out.println("插入10个键值对后，缓存内容：");
+        System.out.println(cache + "\n");
+
+        System.out.println("访问键值为7的节点后，缓存内容：");
+        cache.getOne(6);
+        System.out.println(cache + "\n");
+
+        System.out.println("插入键值为1的键值对后，缓存内容：");
+        cache.save(1, 1);
+        System.out.println(cache);
+    }
+}
